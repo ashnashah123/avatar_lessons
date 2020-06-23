f9a258d79e8690e6f3b4c956b466b3ae639b723b
Ashna Shah <shah.ash@husky.neu.edu>
2020-06-23 15:39:34 -0400

    Updates

---
+++ precache-manifest.5aadedd44cd39258f179f152dd739ba0.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc369f6f5414164ec857e41303bbf75",
+    "url": "/avatar_lessons/index.html"
+  },
+  {
+    "revision": "2a27ab3e3f550a84904b",
+    "url": "/avatar_lessons/static/css/main.916f1331.chunk.css"
+  },
+  {
+    "revision": "7d49fdd0be7c9c93c374",
+    "url": "/avatar_lessons/static/js/2.4df26a81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/avatar_lessons/static/js/2.4df26a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a27ab3e3f550a84904b",
+    "url": "/avatar_lessons/static/js/main.846ba64a.chunk.js"
+  },
+  {
+    "revision": "686914919c9f2e36e375",
+    "url": "/avatar_lessons/static/js/runtime-main.ef5c7d91.js"
+  },
+  {
+    "revision": "76d798eb33ef60028d7fd3064b982736",
+    "url": "/avatar_lessons/static/media/aang.76d798eb.jpg"
+  },
+  {
+    "revision": "22dfba4ab966ba080846d406c28db637",
+    "url": "/avatar_lessons/static/media/advice.22dfba4a.png"
+  },
+  {
+    "revision": "9946fa279b4a9aafe5e2bacfa646184f",
+    "url": "/avatar_lessons/static/media/avatar.9946fa27.jpg"
+  },
+  {
+    "revision": "f8eb89f1599fbcb2f841e845e914af96",
+    "url": "/avatar_lessons/static/media/dancing.f8eb89f1.gif"
+  },
+  {
+    "revision": "c603d5076bb249a70f9d01ca76e14b3d",
+    "url": "/avatar_lessons/static/media/destiny.c603d507.jpg"
+  },
+  {
+    "revision": "cfa030c0842098f2dc001d16df9c6664",
+    "url": "/avatar_lessons/static/media/elements.cfa030c0.jpg"
+  },
+  {
+    "revision": "a4c93b2c88cd030c6ceaacbf35bd7472",
+    "url": "/avatar_lessons/static/media/fail.a4c93b2c.jpg"
+  },
+  {
+    "revision": "7d4a990106b981b751d62ba0d4bf0463",
+    "url": "/avatar_lessons/static/media/feelings.7d4a9901.png"
+  },
+  {
+    "revision": "8de05eafd2aab86e285a3232307a66b1",
+    "url": "/avatar_lessons/static/media/fire.8de05eaf.png"
+  },
+  {
+    "revision": "141322227417ad786464798a81c7f3ae",
+    "url": "/avatar_lessons/static/media/forgiveness.14132222.png"
+  },
+  {
+    "revision": "3b153c22f20c8ad4f6d36e5a9b3047ae",
+    "url": "/avatar_lessons/static/media/friendship.3b153c22.jpg"
+  },
+  {
+    "revision": "419974516c654bcc786cc8d1aeaa727a",
+    "url": "/avatar_lessons/static/media/goodevil.41997451.jpg"
+  },
+  {
+    "revision": "bd0b7ad47340fcd029f83c3675e903c0",
+    "url": "/avatar_lessons/static/media/honor.bd0b7ad4.gif"
+  },
+  {
+    "revision": "99ff7dc4f18d469ed30c2cc31d31dba6",
+    "url": "/avatar_lessons/static/media/hug.99ff7dc4.png"
+  },
+  {
+    "revision": "ba12c48443ad97f70c02e199524c4a6c",
+    "url": "/avatar_lessons/static/media/katara.ba12c484.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avatar_lessons/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a74a5f44f212ff9ee9622b2287072712",
+    "url": "/avatar_lessons/static/media/love.a74a5f44.png"
+  },
+  {
+    "revision": "ed48bf6e821eefc35f5bca2ce9e169b2",
+    "url": "/avatar_lessons/static/media/love.ed48bf6e.jpg"
+  },
+  {
+    "revision": "37a2a8f13aaf02fb09ceb6f4957a8a29",
+    "url": "/avatar_lessons/static/media/lying.37a2a8f1.gif"
+  },
+  {
+    "revision": "a5ec3c54e9cf0e731bf1543cf794be8a",
+    "url": "/avatar_lessons/static/media/monks.a5ec3c54.jpg"
+  },
+  {
+    "revision": "9122594bea6c3399a3f53d67ea9fd1af",
+    "url": "/avatar_lessons/static/media/music.9122594b.png"
+  },
+  {
+    "revision": "db04d160c33aa5f8839872364880c18d",
+    "url": "/avatar_lessons/static/media/nervous.db04d160.png"
+  },
+  {
+    "revision": "6cc9cef0c5ebee3311a6f5102b422afc",
+    "url": "/avatar_lessons/static/media/paintedlady.6cc9cef0.png"
+  },
+  {
+    "revision": "a98869db25df124b0cab2d337740e8dc",
+    "url": "/avatar_lessons/static/media/past.a98869db.png"
+  },
+  {
+    "revision": "2213275ebffd9f120a7282f04e5bb20f",
+    "url": "/avatar_lessons/static/media/positive.2213275e.jpeg"
+  },
+  {
+    "revision": "30e3eb2f3ca632f8ca03b3b63c249115",
+    "url": "/avatar_lessons/static/media/prisoner.30e3eb2f.jpeg"
+  },
+  {
+    "revision": "22d5f43ecc47cd903e3c3b6e41965862",
+    "url": "/avatar_lessons/static/media/sleepy.22d5f43e.png"
+  },
+  {
+    "revision": "17d7e89bf22a04fbe9a5db5f9250fce9",
+    "url": "/avatar_lessons/static/media/spacerock.17d7e89b.png"
+  },
+  {
+    "revision": "036a75cc1986a7f08df166f4210fd1ec",
+    "url": "/avatar_lessons/static/media/tea.036a75cc.jpg"
+  },
+  {
+    "revision": "68318dacce42b08a125cf77fd5c39bb4",
+    "url": "/avatar_lessons/static/media/toph.68318dac.png"
+  },
+  {
+    "revision": "6e8d563242772992921ff8acaef714c3",
+    "url": "/avatar_lessons/static/media/tunnel.6e8d5632.jpg"
+  },
+  {
+    "revision": "0a522fc3b14a122dddfb1ae22c658c63",
+    "url": "/avatar_lessons/static/media/uncle.0a522fc3.gif"
+  },
+  {
+    "revision": "7f4c348bbcbd35545df8743ae17569c5",
+    "url": "/avatar_lessons/static/media/zuko.7f4c348b.png"
+  }
+]);
